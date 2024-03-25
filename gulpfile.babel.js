8752d24b9e6f392e627eaabea60efab78c08dce1
Ian Daly <ineptian@gmail.com>
2024-03-25 10:34:56 -0700

    addd uncss to stage

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -93,7 +93,7 @@ function sass() {
     autoprefixer(),
 
     // UnCSS - Uncomment to remove unused styles in production
-    // PRODUCTION && uncss.postcssPlugin(UNCSS_OPTIONS),
+    PRODUCTION && uncss.postcssPlugin(UNCSS_OPTIONS),
   ].filter(Boolean);
 
   return gulp.src('src/assets/scss/app.scss')
